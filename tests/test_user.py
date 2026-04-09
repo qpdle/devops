7d9fb17e70ca499f9d5b4577ca1ff1c854579b84
Daniil Orlov <orlovdaniil336@gmail.com>
2026-04-09 08:22:37 +0300

    Add tests, workflows, Dockerfile and app

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,45 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'notfound@mail.com'})
+    
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        "name": "New User",
+        "email": "new_user@mail.com"
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)  
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    existing_user = {
+        "name": "Ivan Ivanov",
+        "email": users[0]['email']  
+    }
+
+    response = client.post("/api/v1/user", json=existing_user)
+
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    new_user = {
+        "name": "To Delete",
+        "email": "delete_me@mail.com"
+    }
+    client.post("/api/v1/user", json=new_user)
+
+    response = client.delete("/api/v1/user", params={'email': new_user['email']})
+    assert response.status_code == 204
+
+    response = client.get("/api/v1/user", params={'email': new_user['email']})
+    assert response.status_code == 404
